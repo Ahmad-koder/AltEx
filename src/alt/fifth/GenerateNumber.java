eb601fee3a5688f5a67d0a70ec24c6dd0135de20
AiratKhayrullov <67463538+AiratKhayrullov@users.noreply.github.com>
2021-06-02 08:47:54 +0300

    Update GenerateNumber.java

--- src/alt/fifth/GenerateNumber.java
+++ src/alt/fifth/GenerateNumber.java
@@ -1,33 +1,25 @@
 package alt.fifth;
 
 import java.time.LocalTime;
-import java.util.Arrays;
 
 public class GenerateNumber{
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
 
         int [][] automat = new int[64][63];
 
         LocalTime now = LocalTime.now();
-        int hour = now.getNano();
+
+        int nanoSeconds = now.getNano();
+
+        String bitsNanoSeconds = Integer.toBinaryString(nanoSeconds);
 
 
-        int check = 0;
+        for(int i = 0;i < bitsNanoSeconds.length(); i++)
+            automat[0][i] = Character.getNumericValue(bitsNanoSeconds.charAt(i));
 
-        String bits = Integer.toBinaryString(hour);//Перевод секунд в двоичную форму счисления
 
-        //Вставляем полученное двоичное число в первую строку автомата
-        for(int i = 0;i < 64; i++) {
-            if(check != bits.length()) {
-                automat[0][i] = Character.getNumericValue(bits.charAt(check++));
-            }
-            else
-                break;
-        }
-
-        //генерация всех последующих поколений по правилу
         for (int i = 1; i < automat.length; i++)
             for (int j = 0; j < automat[i].length; j++)
             {
@@ -37,28 +29,25 @@ public class GenerateNumber{
                 automat[i][j] = automat[i - 1][first] ^ (automat[i - 1][j] | automat[i - 1][second]);
             }
 
-        //рандомная точка старта числа в столбце
         int startNumber = (now.getSecond() + now.getHour()+ now.getMinute()) % (63);
 
-        //рандомная длина двоичного кода числа
         int lengthNumber = (now.getNano() / now.getSecond()) % (63-startNumber);
 
 
-        //int lengthNumber = 7;
+        //int lenghtNumber = 7;
 
         int [] number = new int [lengthNumber+1];
 
-        //Копируем число из центрального столбца автомата
         for(int i = 0; i < number.length; i++) {
             number[i] = automat[25][startNumber++];
         }
 
-        StringBuilder newNumber = new StringBuilder();
+        StringBuilder numberString = new StringBuilder();
 
-        //образуем строку для удобного перевода
         for (int value : number)
-            newNumber.append(value);
-        System.out.println(Long.parseLong(newNumber.toString(), 2));
+            numberString.append(Integer.toString(value));
+
+        System.out.println(Long.parseLong(numberString.toString(), 2));
 
     }
 }
