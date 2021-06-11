dfb532dfbe6fcfa7dcb63b79f726e2c4a88801f7
AiratKhayrullov <67463538+AiratKhayrullov@users.noreply.github.com>
2021-06-11 08:47:01 +0400

    Update GenerateNumber.java

--- src/alt/fifth/GenerateNumber.java
+++ src/alt/fifth/GenerateNumber.java
@@ -2,52 +2,48 @@ package alt.fifth;
 
 import java.time.LocalTime;
 
-public class GenerateNumber{
-
+public class GenerateNumber {
 
     public static void main(String[] args) {
 
-        int [][] automat = new int[64][63];
+        int[][] automat = new int[64][63];
 
         LocalTime now = LocalTime.now();
 
-        int nanoSeconds = now.getNano();
+        String bitsNanoSeconds = Integer.toBinaryString(now.getNano());
 
-        String bitsNanoSeconds = Integer.toBinaryString(nanoSeconds);
-
-
-        for(int i = 0;i < bitsNanoSeconds.length(); i++)
+        for (int i = 0; i < bitsNanoSeconds.length(); i++)
             automat[0][i] = Character.getNumericValue(bitsNanoSeconds.charAt(i));
 
-
         for (int i = 1; i < automat.length; i++)
-            for (int j = 0; j < automat[i].length; j++)
-            {
+
+            for (int j = 0; j < automat[i].length; j++) {
                 int first = (j - 1 + automat[i].length) % automat[i].length;
+
                 int second = (j + 1 + automat[i].length) % automat[i].length;
 
                 automat[i][j] = automat[i - 1][first] ^ (automat[i - 1][j] | automat[i - 1][second]);
             }
 
-        int startNumber = (now.getSecond() + now.getHour()+ now.getMinute()) % (63);
+        int startNumber = Math.abs(((int) Runtime.getRuntime().freeMemory() - now.getHour() + now.getNano()) % (63));
 
-        int lengthNumber = (now.getNano() / now.getSecond()) % (63-startNumber);
-
+        int lengthNumber = Math.abs((((int) Runtime.getRuntime().totalMemory() - now.getSecond()) * now.getMinute()) % (63 - startNumber));
 
         //int lenghtNumber = 7;
 
-        int [] number = new int [lengthNumber+1];
+        int[] number = new int[lengthNumber + 1];
 
-        for(int i = 0; i < number.length; i++) {
+        for (int i = 0; i < number.length; i++) {
             number[i] = automat[25][startNumber++];
         }
 
         StringBuilder numberString = new StringBuilder();
 
         for (int value : number)
-            numberString.append(Integer.toString(value));
+            numberString.append(value);
 
         System.out.println(Long.parseLong(numberString.toString(), 2));
 
     }
 }
+
